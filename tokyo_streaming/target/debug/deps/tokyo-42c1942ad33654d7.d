4603a87585cae2688f3cc04b2cc27e9f34ef57af
Wesley Parr <rustchain64@gmail.com>
2021-04-17 20:31:04 -0600

    tokio treaming lean init

---
+++ tokyo_streaming/target/debug/deps/tokyo-42c1942ad33654d7.d
@@ -0,0 +1,5 @@
+/home/rustchain64/projects/tokyo_starter/tokyo/target/debug/deps/tokyo-42c1942ad33654d7: src/main.rs
+
+/home/rustchain64/projects/tokyo_starter/tokyo/target/debug/deps/tokyo-42c1942ad33654d7.d: src/main.rs
+
+src/main.rs:
---
+++ tokyo_streaming/target/debug/tokyo.d
@@ -0,0 +1,1 @@
+/home/rustchain64/projects/tokyo_starter/tokyo/target/debug/tokyo: /home/rustchain64/projects/tokyo_starter/tokyo/src/main.rs
